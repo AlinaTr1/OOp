9f4d01c93920de43a210bb039ec8e07bdc72a970
Алина Троян <gr433_trase@lab-402-8.tomtit-tomsk.ru>
2024-12-14 11:19:42 +0700

    second

--- locallibrary/locallibrary/catalog/admin.py
+++ locallibrary/locallibrary/catalog/admin.py
@@ -4,20 +4,23 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance
 
-#admin.site.register(Book)
-#admin.site.register(Author)
 admin.site.register(Genre)
-#admin.site.register(BookInstance)
 
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BooksInline(admin.StackedInline):
+    model = Book
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 admin.site.register(Author, AuthorAdmin)
 
-class BooksInstanceInline(admin.TabularInline):
-    model = BookInstance
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
